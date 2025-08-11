1a9ae5b73db3c6bad2a8c2d8f55f00d4d342d24d
anilkumar72-engg <anilkumarbudha72@gmail.com>
2025-08-11 15:35:08 +0530

    script.js

--- script.js
+++ script.js
@@ -4,5 +4,10 @@ AOS.init({
 });
 
 function toggleMobileMenu() {
-  document.getElementById("mobileDropdown").classList.toggle("hidden");
+  const dropdown = document.getElementById("mobileDropdown");
+  dropdown.classList.toggle("hidden");
+
+  const btn = document.querySelector('#mobileMenu button');
+  const expanded = btn.getAttribute('aria-expanded') === 'true' || false;
+  btn.setAttribute('aria-expanded', !expanded);
 }
